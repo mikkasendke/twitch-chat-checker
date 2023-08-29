1851b100fa3f017dbb32b7bc7af590bda7dcb657
Mikka <mikka@sendke.eu>
2023-08-29 17:56:22 +0200

    proxy support and handled potential error when channel does not exist.

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import { startBrowser } from './browser.js';
 import { Scraper } from './pageScraper.js';
 
 const config = {
-    channel: 'Channel to search in (not the URL, just the name, e.g. "xQcOW" not "https://www.twitch.tv/xqcow")',
-    search: 'Username to search for in chat',
+    channel: '', // Channel to search in (not the URL, just the name, e.g. 'xQcOW' or 'xqcow' not 'https://www.twitch.tv/xqcow')
+    search: '', // Username to search for in chat
     headless: true, // true = no browser window, false = browser window
     limit: -1, // -1 = no limit
-    delay: 5000 // delay between each check in ms, be careful with this.
+    delay: 5000, // delay between each check in ms, be careful with this.
+    prepend_url: "" // prepend url if you want to use a proxy.
 }
 
 console.log(`Checking if ${config.search} is in the chat of ${config.channel}.`)
@@ -19,17 +20,23 @@ setInterval(async () => {
         process.exit(0);
     }
     config.limit--;
-    const scraper = new Scraper(browserInstance, `https://www.twitch.tv/popout/${config.channel.toLowerCase()}/chat?popout=`);
+    const scraper = new Scraper(browserInstance, `${config.prepend_url}https://www.twitch.tv/popout/${config.channel.toLowerCase()}/chat?popout=`);
     await scraper.scrape(callback);
 
 }, config.delay);
 
 
 const callback = async (page) => {
-    // click viewer list button
-    await page.click('[data-test-selector="chat-viewer-list"]');
-    // wait for the list to load
-    await page.waitForSelector('[aria-labelledby="chat-viewers-list-header-VIPs"]');
+
+    try {
+        // click on the viewer list button and wait for it to load.
+        await page.click('[data-test-selector="chat-viewer-list"]');
+        await page.waitForSelector('[aria-labelledby="chat-viewers-list-header-VIPs"]');
+    }
+    catch (e) {
+        console.log('Couldn\'t find viewer list, exiting.');
+        process.exit(1);
+    }
 
     const usernames = await page.evaluate(() => {
         let users = [];
